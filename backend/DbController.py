8bc5fe22f53184d31c3d1202a68ba469ab515cad
tgmerge <tgmgsp@gmail.com>
2014-04-10 11:46:01 +0800

    added multithread fetching

--- backend/DbController.py
+++ backend/DbController.py
@@ -6,6 +6,7 @@ import threading
 import PriceParser
 import sys
 import time
+import json
 
 
 def threaded(fn):
@@ -25,13 +26,31 @@ class DbController(object):
     dbFilename = "soda.db"
     conn = None
     dbInitSql = '''
-        CREATE TABLE IF NOT EXISTS Updates(q TEXT, time TEXT);
-        CREATE TABLE IF NOT EXISTS Items(id TEXT, site TEXT, url TEXT, price FLOAT, img TEXT, title TEXT, q TEXT);
-        CREATE TABLE IF NOT EXISTS Sessions(sid TEXT, data TEXT);
+        CREATE TABLE IF NOT EXISTS
+            Updates(q    TEXT PRIMARY KEY,
+                    time TEXT);
+
+        CREATE TABLE IF NOT EXISTS
+            Items(q     TEXT NOT NULL,
+                  id    TEXT NOT NULL,
+                  site  TEXT,
+                  url   TEXT,
+                  price FLOAT,
+                  img   TEXT,
+                  title TEXT,
+                  PRIMARY KEY(q, id));
+
+        CREATE TABLE IF NOT EXISTS
+            Sessions(sid TEXT PRIMARY KEY,
+                     data TEXT);
+
+        CREATE INDEX Items_index ON
+            Items(q, id);
         '''
-    maxFetchPages = 5
-    minDelayBetweenFetches = 10
-    waitTimeForFetch = 3
+    maxFetchPages = 5 # 5 default
+    minDelayBetweenFetches = 30 # 10 default
+    waitTimeForFetch = 3 # 3 default
+    itemsPerPage = 10 # 20 default
 
     def __init__(self, dbFilename):
         """Initialize the database with given filename.
@@ -45,9 +64,16 @@ class DbController(object):
 
     # connection manage
 
+    def dictFactory(self, cursor, row):
+        """[{'key': value, 'key': value}, {...}, ...]"""
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
     def connDb(self):
         self.conn = sqlite3.connect(self.dbFilename)
-        self.conn.row_factory = sqlite3.Row
+        self.conn.row_factory = self.dictFactory
         self.conn.text_factory = str
         print '[connDb]Connection established'
 
@@ -95,64 +121,69 @@ class DbController(object):
         cur = self.conn.execute(sql)
         return cur.fetchall()
 
-    def insertTuples(self, table, values):
+    def insertTuples(self, table, values, replace=False):
         """Insert values into table.
         If the table has only one key USE (VAL,) INSTEAD OF (VAL)!!!
         args:
             conn - connection to db
             table - string, table name
             values - list of tuple(val, val, ...)
-        return:
-            succ or not
         """
         cur = self.conn.cursor()
         placeholders = "(%s)" % ("?,"*(values[0].__len__()-1) + "?")
-        sql = "INSERT INTO %s VALUES %s" % (table, placeholders)
+        sql = "INSERT %s INTO %s VALUES %s" % (replace and "OR REPLACE" or "", table, placeholders)
         cur.executemany(sql, values)
         self.conn.commit()
-        print '[insertTuples]insert into %s done' % table
+        print '[insertTuples]insert%s into %s done' % (replace and " and replace" or "", table)
 
     # request handler
 
     def queryUpdate(self, q):
         """return dict(q, time) or None(if not found)"""
         result = self.findTuple("*", "Updates", ["q = '%s'" % q])
-        uptime = result and result[0][1] or "0"
-        return {"q": q, "time": uptime}
+        uptime = result and result[0]["time"] or "0"
+        return {"q": q,
+                "updateTime": uptime}
 
     def querySearch(self, q, site, sid, page):
         """return a list of results(q, site, sid, page)"""
         """TODO"""
-        sessionRes = self.findTuple("data", "Sessions", ["sid = '%s'" % sid])
-        print "### 1",
-        print sessionRes
-        self.fetchAndStore(q, site)
+        page = int(page)
+        site = site.capitalize()
 
-        if not sessionRes:
-            itemsRes = self.findTuple("*", "Items", ["q = '%s'" % q, "site = '%s'" % site])
-            print "### 2",
-            print itemsRes
-            if not itemsRes:
+        self.fetchAndStore(q, site)
+        print "[querySearch]Sent fetchAndStore(%s, %s)" % (q, site)
+
+        print "[querySearch]Try to fetch from Sessions...",
+        fromSessions = self.findTuple("data", "Sessions", ["sid = '%s'" % sid])
+        items = fromSessions and json.loads(fromSessions[0]["data"]) or None
+        print items and "OK" or "Failed"
+
+        if not items:
+            print "[querySearch]Try to fetch from Items...",
+            items = self.findTuple("*", "Items", ["q = '%s'" % q, "site = '%s'" % site])
+            print items and "OK" or "Failed"
+            if not items:
+                print "[querySearch]Waiting...",
                 time.sleep(self.waitTimeForFetch)
-                itemsRes = self.findTuple("*", "Items", ["q = '%s'" % q, "site = '%s'" % site])
-                print "### 3",
-                print itemsRes
-            # write itemsres to sessions
-            sessionRes = self.findTuple("data", "Sessions", ["sid = '%s'" % sid])
-            print "### 4",
-            print sessionRes
-        # fine. result in sessionres.
-        # paging, paging
-        return sessionRes
+                items = self.findTuple("*", "Items", ["q = '%s'" % q, "site = '%s'" % site])
+                print items and "re-search OK" or "re-search Failed"
+                self.insertTuples("Sessions", [(sid, json.dumps(items))], replace=True)
+
+        pagedItems = items[(page-1)*self.itemsPerPage:page*self.itemsPerPage]
+        print "[querySearch]Total %d items, paged from %d to %d" % (items.__len__(), (page-1)*self.itemsPerPage, (page)*self.itemsPerPage-1)
+        return {"q": q,
+                "site": site,
+                "session": sid,
+                "page": page,
+                "count": pagedItems.__len__(),
+                "totalPage": items.__len__()/self.itemsPerPage,
+                "items": pagedItems}
 
     def renewUpdate(self, q):
         """renew update time for keyword q"""
-        sql = "DELETE FROM Updates WHERE q = '%s';" % q
-        self.conn.execute(sql)
-        self.conn.commit()
-
         currentTime = time.time()
-        self.insertTuples("Updates", [(q, currentTime)])
+        self.insertTuples("Updates", [(q, currentTime)], replace=True)
         print "[renewUpdate]Uptime of %s is %s now" % (q, currentTime)
 
     # data fetch
@@ -164,18 +195,19 @@ class DbController(object):
         ''' todo fetch each site!'''
         # get controller
         controller = DbController(self.dbFilename)
-        updateTime = int(float(controller.queryUpdate(q)["time"]))
+        updateTime = int(float(controller.queryUpdate(q)["updateTime"]))
         currentTime = int(float(time.time()))
+        print "[fetchAndStore]Time from last query is %d" % (currentTime-updateTime)
         if currentTime-updateTime < self.minDelayBetweenFetches:
-            print "[fetchAndStore]Time from last query is %s, skip" % q
+            print "[fetchAndStore]skip"
             pass
         else:
+            controller.renewUpdate(q)
             parser = PriceParser.PriceParser()
             for page in range(1, self.maxFetchPages + 1):
                 results = parser.parseEverything(q, page, site)
                 if results.__len__() > 0:
-                    controller.insertTuples("Items", [(item['pid'], item['site'], item['url'], item['price'], item['img'], item['title'], q) for item in results])
-            controller.renewUpdate(q)
+                    controller.insertTuples("Items", [(q, item['pid'], item['site'], item['url'], item['price'], item['img'], item['title']) for item in results], replace=True)
             print "[fetchAndStore]Query %s from %s done" % (q, site)
 
         controller.closeDb()
